3307b1544044ae68ac3a213b84b4fa8b22b8ebd6
thabemmz <info@cvbwebdesign.nl>
2015-11-29 17:01:52 +0100

    Remove comment to make parseosxdefaults more readable

--- helpers/parseosxdefaults.sh
+++ helpers/parseosxdefaults.sh
@@ -12,10 +12,6 @@ function print_defaultswrite_for() {
     if [[ $line == "{" || $line == "}" ]]; then
       continue
     fi
-    # the construction below may be kind-a vague but it does the following:
-    # 1. tr '=' ' ' <<< $line replaces each '=' to a space in $line
-    # 2. This is wrapped in $(), which outputs the outcome of tr ran in a subshell
-    # 3. This is wrapped in (), which turns it into an array, items separated by space
     KEYVAL=($(tr '=' ' ' <<< $line))
     KEY=${KEYVAL[0]}
     VAL=${KEYVAL[1]//;}
